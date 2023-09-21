d7683ec25aa2dd65ae12d974627e1c4980585df4
5h4rk-lab <sharkmagic07@gmail.com>
2023-09-21 15:12:41 -0400

    second commit

---
+++ src/Components/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './home.css';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className='homepage'>
+                <h1>PlayHard</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
